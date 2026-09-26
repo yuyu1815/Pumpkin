b7b359bf89ee4f541105cdb4fd872c66b4194310
yuzumican <83267351+yuyu1815@users.noreply.github.com>
2026-09-26 15:39:47 +0900

    chore(net): remove unused NonZero import (#13)

--- crates/pumpkin/src/net/java/play/mod.rs
+++ crates/pumpkin/src/net/java/play/mod.rs
@@ -2,7 +2,6 @@ use pumpkin_util::{Hand, PermissionLvl};
 use rsa::pkcs1v15::{Signature as RsaPkcs1v15Signature, VerifyingKey};
 use rsa::signature::Verifier;
 use sha1::Sha1;
-use std::num::NonZero;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::time::{SystemTime, UNIX_EPOCH};
